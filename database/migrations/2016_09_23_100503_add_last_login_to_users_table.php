80b030f188f07723234ea139e35d5810f218f826
yong <yongil1206@naver.com>
2016-09-23 18:53:17 +0900

    kylehyun

--- database/migrations/2016_09_23_100503_add_last_login_to_users_table.php
+++ database/migrations/2016_09_23_100503_add_last_login_to_users_table.php
@@ -12,7 +12,7 @@ class AddLastLoginToUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function(Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->timestamp('last_login')->nullable();
         });
     }
@@ -24,7 +24,7 @@ class AddLastLoginToUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('users', function(Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('last_login');
         });
     }
